d887e85b82872e58714ed064e7e69e7714a18157
thanga-111 <tt8772@srmist.edu.in>
2026-04-21 13:03:31 +0530

    Add HA_UC2 source

---
+++ HA_UC2.java
@@ -0,0 +1,10 @@
+public class HA_UC2 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");    
+        }
+    }
+}
